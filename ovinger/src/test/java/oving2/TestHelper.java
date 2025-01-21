e69623f99808025987be65cad378223fa03fceb9
Jon P. Hallem <128969752+jonp-h@users.noreply.github.com>
2025-01-21 12:49:01 +0100

    feat: add TestHelper class for field privacy checks

---
+++ ovinger/src/test/java/oving2/TestHelper.java
@@ -0,0 +1,16 @@
+package oving2;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import org.junit.jupiter.api.Assertions;
+
+public class TestHelper {
+
+    public static void checkIfFieldsPrivate(Class<?> class1) {
+        for (Field field : class1.getDeclaredFields()) {
+            Assertions.assertTrue(Modifier.isPrivate(field.getModifiers()),
+                    "Expected field " + field.getName() + " to be private!");
+        }
+    }
+}
